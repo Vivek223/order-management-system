47cb55a8b296d6736b4b5836d3c604f1961ff9bf
viv_tiw <tiwarivivek23@gmail.com>
2024-07-05 18:59:06 +0300

    Project setup

---
+++ src/main/java/com/oms/App.java
@@ -0,0 +1,6 @@
+package com.oms;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("coming..");
+    }
+}
---
+++ src/test/java/Test2Test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Test2Test {
+
+    @Test
+    public void test1(){
+        System.out.println("test something..");
+    }
+}
