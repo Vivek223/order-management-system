a01b102ff7154bb463afc1ae55ff1413584df2ce
viv_tiw <tiwarivivek23@gmail.com>
2024-07-06 18:28:56 +0300

    Added controller and rest end points

--- src/main/java/com/oms/controller/OrderController.java
+++ src/main/java/com/oms/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.oms.controller;
 
 import com.oms.models.Order;
 import com.oms.repos.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,9 @@ import java.util.List;
 @RestController
 public class OrderController {
 
-    OrderRepository orderRepository;
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    private OrderRepository orderRepository;
 
     public OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -20,12 +24,15 @@ public class OrderController {
 
     @PostMapping("/create-multiple-order")
     public ResponseEntity<List<Order>> createMultipleOrders(@RequestBody List<Order> orders) {
+        logger.info("enter create multiple order");
+
         List<Order> savedOrders = orderRepository.saveAll(orders);
         return new ResponseEntity<>(savedOrders, HttpStatus.CREATED);
     }
 
     @PostMapping("/generate-random-orders")
     public ResponseEntity<List<Order>> generateRandomOrders(@RequestParam int n) {
+        logger.info("enter generate random orders");
         List<Order> randomOrders = new ArrayList<>();
         for(int i=0; i<n;i++){
             Order order = new Order("comment# " + i, "ipid #" + i);
